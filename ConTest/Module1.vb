66c06ada0f487fa676d2fafef45cd42e26c32ddf
DancingDolphins <36990113@qq.com>
2019-09-08 16:07:30 +0800

    0.90

--- ConTest/Module1.vb
+++ ConTest/Module1.vb
@@ -1,7 +1,23 @@
-﻿Module Module1
+﻿Imports RosApi
+
+Module Module1
+    Private ROS As New RosApi.Api
+
 
     Sub Main()
+        Dim Auth As New RosAuth
+        Auth.IP = "192.168.10.49"
+        Auth.Port = 8728
+        Auth.Username = "admin"
+        Auth.Password = "admin"
+        ROS.Log = AddressOf Log
 
+        ROS.Connect(Auth)
+        ROS.Login()
+    End Sub
+
+    Private Sub Log(ByVal Str As String)
+        Console.WriteLine(Str)
     End Sub
 
 End Module
